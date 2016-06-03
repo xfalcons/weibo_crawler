d8aacf4661914bcc36d0cbfe4ff7f7ce0d1024c6
Kevin Luo <kevin_luo@miiicasa.com>
2016-06-03 14:27:40 +0800

    fix bug

--- go.sh
+++ go.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 host=`hostname`
-if [ "$host" = "a01.spider.logstat.qingdao.youku"]; then
+if [ "$host" = "a01.spider.logstat.qingdao.youku" ]; then
 	cd /home/kevin_luo/dev/weibo_crawler
+	echo "Running on beta env"
 else
 	cd /home/up/weibo_crawler
 	source ./bin/activate
 	cd weibo_crawler
 	export LANG=en_US.UTF8
+	echo "Running on production env"
 fi
 
 ./weibo_topic.py --debug --page 10
