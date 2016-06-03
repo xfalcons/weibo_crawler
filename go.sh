d5235bbe670c50d4db2400bc4eb77282857a8881
Kevin Luo <kevin_luo@miiicasa.com>
2016-06-03 14:25:02 +0800

    cd to different dir on different environment

--- go.sh
+++ go.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-cd /home/kevin_luo/dev/weibo_crawler
-source ./bin/activate
-export LANG=en_US.UTF8
+host=`hostname`
+if [ "$host" = "a01.spider.logstat.qingdao.youku"]; then
+	cd /home/kevin_luo/dev/weibo_crawler
+else
+	cd /home/up/weibo_crawler
+	source ./bin/activate
+	cd weibo_crawler
+	export LANG=en_US.UTF8
+fi
+
 ./weibo_topic.py --debug --page 10
 ./weibo_hot.py --debug
 ./weibo_search.py --debug
-deactive
